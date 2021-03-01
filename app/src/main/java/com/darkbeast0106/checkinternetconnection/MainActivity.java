0f2f6c3977fdb414caff7226cc14bd48f062edc1
Budaházi Bence <budahazi.bence@petrik.hu>
2021-03-01 16:57:11 +0100

    Check Connection

--- app/src/main/java/com/darkbeast0106/checkinternetconnection/MainActivity.java
+++ app/src/main/java/com/darkbeast0106/checkinternetconnection/MainActivity.java
@@ -2,13 +2,83 @@ package com.darkbeast0106.checkinternetconnection;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textInternet;
+    ConnectivityManager connectivityManager;
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    @Override
+    protected void onResume() {
+        timer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                textInternet.setVisibility(checkConnected() ? View.VISIBLE : View.GONE);
+            }
+        };
+        timer.schedule(task, 0,5000);
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        timer.cancel();
+        super.onPause();
+    }
+
+    // A hostAvailable függvény elég lenne,
+    // de mobil adatforgalmat spórolunk azzal, hogy feltételezzük,
+    // hogy ha van mobil kapcsolat akkor internet is van.
+    private boolean checkConnected() {
+        NetworkInfo.State mobilState
+                = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState();
+        NetworkInfo.State wifiState
+                = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState();
+        if (mobilState.equals(NetworkInfo.State.CONNECTED)){
+            return true;
+        }
+        if (wifiState.equals(NetworkInfo.State.CONNECTED)){
+            return hostAvailable("www.google.com", 80);
+        }
+        return false;
+    }
+
+    public boolean hostAvailable(String host, int port) {
+        try (Socket socket = new Socket()) {
+            socket.connect(new InetSocketAddress(host, port), 2000);
+            return true;
+        } catch (IOException e) {
+            // Either we have a timeout or unreachable host or failed DNS lookup
+            System.out.println(e);
+            return false;
+        }
+    }
+
+    private void init() {
+        textInternet = findViewById(R.id.text_internet);
+        connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
     }
 }
